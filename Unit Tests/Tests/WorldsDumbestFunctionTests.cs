322bef6ce15a4f346fde658ef962bc5325657026
EduardoAquinta <edwoodashton@yahoo.co.uk>
2024-01-09 11:54:56 +0000

    first commit

---
+++ Unit Tests/Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Tests.Tests {
+    public class WorldsDumbestFunctionTests {
+        public static void WorldsDumbestFunction_ReturnsPikaChuIfZero_ReturnsString() {
+            try {
+                //Arrange - Go get your variables, what you need, classes etc.
+                int num = 0;
+                WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+                //Act - execute the function
+                string result = worldsDumbest.ReturnsPokemonIfZero(num);
+                //Assert - whatever is returned, is it what you want?
+                if (result == "PICKACHU") {
+                    Console.WriteLine("Passed : WorldsDumbestFunction_ReturnsPikachuIfZero_ResturnsString");
+                } else {
+                    Console.WriteLine("FAILED: WorldsDumbestFunction_ReturnsPikachuIfZero_ResturnsString");
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Unit Tests/WorldsDumbestFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Tests {
+    public class WorldsDumbestFunction 
+    {
+        public string ReturnsPokemonIfZero(int num) 
+        {
+            if(num == 0) {
+                return "PICKCHU!";
+            } else {
+                return "Squirtle";
+            }
+        }
+    }
+}
